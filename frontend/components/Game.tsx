31d4a193d3612db01be01f0e3299a558246cf5ad
masatsch <kingmasatojames@gmail.com>
2023-04-01 22:56:08 -0700

    play state

--- frontend/components/Game.tsx
+++ frontend/components/Game.tsx
@@ -1,26 +1,24 @@
-import { Box, Button, Grid } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { useAppProvider } from "../context/AppContext";
 
 interface GameProps {
-  onConnectWallet: () => void;
   onPlay: () => void;
+  onRefresh: () => void;
 }
 
-export const Game: React.FC<GameProps> = ({ onConnectWallet, onPlay }) => {
+export const Game: React.FC<GameProps> = ({ onPlay, onRefresh }) => {
   const [state] = useAppProvider();
   return (
-    <Box>
-      <Grid templateColumns="repeat(8, 1fr)" gap={1}>
-        {/* 8x8 grid cells */}
-        {Array.from({ length: 64 }, (_, i) => (
-          <Box key={i} bg="gray.200" h="40px" w="40px" />
-        ))}
-      </Grid>
-      <Button onClick={onConnectWallet} mt={4}>
-        {state.address ?? "Connect Wallet"}
-      </Button>
+    <Box margin="20px auto">
       <Button onClick={onPlay} mt={4} ml={4}>
-        Play
+        {state.gamePlayState.hider === state.address
+          ? "hider"
+          : state.gamePlayState.seeker === state.address
+          ? "seeker"
+          : "play"}
+      </Button>
+      <Button onClick={onRefresh} mt={4} ml={4}>
+        refresh
       </Button>
     </Box>
   );
--- frontend/context/AppContext.tsx
+++ frontend/context/AppContext.tsx
@@ -1,26 +1,41 @@
-import { createContext, useContext, useState } from 'react';
-import { ethers } from 'ethers';
-import { HideAndSeek as HideAndSeekType } from '../types/abis/HideAndSeek';
+import { createContext, useContext, useState } from "react";
+import { HideAndSeek as HideAndSeekType } from "../types/abis/HideAndSeek";
+
+interface GamePlayState {
+  hider: string | null;
+  seeker: string | null;
+  isPlaying: boolean;
+  coodination: {
+    x: number;
+    y: number;
+  };
+}
 
 interface AppState {
-  provider: ethers.providers.Web3Provider | null;
-  signer: ethers.Signer | null;
-  address: string | null;
+  address: string | undefined;
   hideAndSeek: HideAndSeekType | null;
-  gamePlayState: any; // gamePlayStateの型を適切に定義してください
+  gamePlayState: GamePlayState; // gamePlayStateの型を適切に定義してください
 }
 
 interface AppProviderProps {
   children: React.ReactNode;
 }
 
-const AppContext = createContext<[AppState, React.Dispatch<React.SetStateAction<AppState>>]>([
+const AppContext = createContext<
+  [AppState, React.Dispatch<React.SetStateAction<AppState>>]
+>([
   {
-    provider: null,
-    signer: null,
-    address: null,
+    address: undefined,
     hideAndSeek: null,
-    gamePlayState: null,
+    gamePlayState: {
+      hider: null,
+      seeker: null,
+      isPlaying: false,
+      coodination: {
+        x: 0,
+        y: 0,
+      },
+    },
   },
   () => {},
 ]);
@@ -29,12 +44,22 @@ export const useAppProvider = () => useContext(AppContext);
 
 export const AppProvider = (props: AppProviderProps) => {
   const [state, setState] = useState<AppState>({
-    provider: null,
-    signer: null,
-    address: null,
+    address: undefined,
     hideAndSeek: null,
-    gamePlayState: null,
+    gamePlayState: {
+      hider: null,
+      seeker: null,
+      isPlaying: false,
+      coodination: {
+        x: 0,
+        y: 0,
+      },
+    },
   });
 
-  return <AppContext.Provider value={[state, setState]}>{props.children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={[state, setState]}>
+      {props.children}
+    </AppContext.Provider>
+  );
 };
